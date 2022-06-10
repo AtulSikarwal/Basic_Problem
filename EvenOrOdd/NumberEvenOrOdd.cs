7c6041d0944c80417fe43d2a8997decacfd98fbd
AtulSikarwal <atulsikarwal5.com>
2022-06-10 14:14:39 +0530

    The code for Number is even or odd

---
+++ EvenOrOdd/NumberEvenOrOdd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Program
+{
+    internal class NumberEvenOrOdd
+    {
+        public void EvenOdd()
+        {
+            Console.WriteLine("Enter the Number :");
+            int Num=Convert.ToInt32(Console.ReadLine());
+            while (Num < 0)
+            {
+                Console.WriteLine("please enter vaild input above 0 ");
+                Num=Convert.ToInt32(Console.ReadLine());
+            }
+            if(Num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
